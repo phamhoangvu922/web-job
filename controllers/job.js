5c76557bcc9f1adadc1feb5611d9b9c1ccaf1e7c
nntrung <nntrung0403@gmail.com>
2020-08-24 05:07:58 +0700

    Chat use messenger fage facebook

--- controllers/job.js
+++ controllers/job.js
@@ -3,5 +3,5 @@ module.exports.job = async (req, res, next) => {
     const id = req.params.id;
     const result = await jobs.getByID(id);
    // console.log(result);
-    res.render('job_details',{jobs: result[0], congty: result[1]});
+    res.render('job_details',{jobs: result[0], congty: result[1],username: req.user, isLogin: req.isAuthenticated()});
 }
